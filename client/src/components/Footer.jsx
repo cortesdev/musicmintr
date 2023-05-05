ab0e54575c23e549b80a984d70252800260e141a
Ricardo Côrtes <giralander@hotmail.com>
2023-05-05 19:24:14 +0200

    Added new footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,13 +3,13 @@ import { logo } from "../assets";
 
 const Footer = () => {
   return (
-    <div className="mt-auto flex   mb-[35px] gap-6 flex flex-col ">
+    <div className="mt-auto flex  text-black dark:text-white  mb-[35px] gap-6 flex flex-col ">
       <hr />
       <div className="flex hero-card max-w-[50px] pr-[20px] mb-2">
         <img src={logo} className="" alt="logo" />
         <h2 className="font-bold ml-3 text-lg">MusikMintr</h2>
       </div>
-      <small className=" text-black dark:text-white">
+      <small className=" ">
         Developed by Ricardo Cortes-
         <a rel="stylesheet" href="http://cortesdev.de">
           cortesdev.de
